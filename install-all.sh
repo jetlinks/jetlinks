e5e6cd02de691c439497933bc9abf1095906deae
zhouhao <zh.sqy@qq.com>
2019-03-15 16:45:43 +0800

    优化脚本

--- install-all.sh
+++ install-all.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-mvn clean install -DskipTests
-cd jetlinks-protocol
-mvn clean install -DskipTests
+mvn clean deploy -DskipTests
+cd coap-codec
+mvn clean deploy -DskipTests
+cd ../jetlinks-protocol
+mvn clean deploy -DskipTests
 cd ../jetlinks-registry
-mvn clean install -DskipTests
+mvn clean deploy -DskipTests
 cd ../jetlinks-gateway
-mvn clean install -DskipTests
+mvn clean deploy -DskipTests
